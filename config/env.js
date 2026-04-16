8b22315d4c653e5f19f94652c667c8bf4b507b98
shuayb mohammed <shuaybmohammed244@gmail.com>
2026-04-16 22:18:17 +0300

    add JWT_SECRET and JWT_EXPIRES_IN to environment variables; update package.json and package-lock.json for bcryptjs and jsonwebtoken dependencies

--- config/env.js
+++ config/env.js
@@ -1,3 +1,3 @@
 import { config } from "dotenv";
 config({path:`.env.${process.env.NODE_ENV || "development"}.local`});
-export const { PORT, DB_URI ,NODE_ENV} = process.env;
+export const { PORT, DB_URI, JWT_SECRET, JWT_EXPIRES_IN, NODE_ENV } = process.env;
