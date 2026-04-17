013a57ab5616d2e2039ed0a3b1f10ef1f8f22161
shuayb mohammed <shuaybmohammed244@gmail.com>
2026-04-17 22:12:49 +0300

    refactor error handling middleware: move to separate file and update import path

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 import express from "express";
-import cookieParser from "cookie-parser";
 import { PORT } from "./config/env.js";
 import usersRouter from "./routes/usersRoutes.js";
 import authRouter from "./routes/authRoutes.js";
 import subscriptionRouter from "./routes/subscriptionRoutes.js";
 import connectDB from "./database/mongodb.js";
-import errorMiddleware from "./models/middleware/errorMiddlware.js";
+import errorMiddleware from "./middleware/errorMiddlware.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use("/api/users", usersRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/subscriptions", subscriptionRouter);
