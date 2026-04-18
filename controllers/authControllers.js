b722a9749704ddf55769a8e37f7ac6757482491d
shuayb mohammed <shuaybmohammed244@gmail.com>
2026-04-18 22:08:24 +0300

    implement signUp and signIn functions in authControllers with error handling and JWT token generation

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,6 +1,59 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs"
+import User from "../models/usersModel.js"
+import jwt from "jsonwebtoken"
+import { JWT_SECRET ,JWT_EXPIRES_IN } from "../config/env.js"
 
-const signup=async (req, res, next) => {
+export const signUp = async (req, res, next) => {
   const session = await mongoose.startSession()
   session.startTransaction()
+  try{
+    const { name, email, password } = req.body
+    const existingUser = await User.findOne({ email })
+    if (existingUser) {
+      const error = new Error("User already exists")
+      error.status = 400
+      throw error
+    }
+    
+    const hashedPassword = await bcrypt.hash(password, 10)
+    
+    const newUsers = await User.create([{ name, email, password: hashedPassword }], { session })
+    const token =jwt.sign({ userId: newUsers[0]._id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN })
+
+    await session.commitTransaction()
+    session.endSession()
+    res.status(201).json({ success : true , message : "User created successfully", data: { token,user:newUsers[0]
+     } })
+  }catch(error){
+    await session.abortTransaction()
+    next(error)
+  }finally{
+    session.endSession()
+  }
 }
+export const signIn = async (req, res, next) => {
+  try{
+    const { email, password } = req.body
+    const user = await User.find({ email })
+    if(!user){
+      const error = new Error("user not found")
+      error.status = 401
+      throw error
+    }
+   
+    const isMatch = await bcrypt.compare(password, user.password)
+    if(!isMatch){
+      const error = new Error("Invalid passward")
+      error.status = 401
+      throw error
+    }
+    
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN })
+    res.json({ success: true, message: "Signed in successfully", data: { token, user } })
+  }catch(error){
+    next(error)
+  }
+}
+export const signOut = async (req, res, next) => {}
+
