7b43ea9f3346ecc3786fc66928c9af90c1d8ee91
shuayb mohammed <shuaybmohammed244@gmail.com>
2026-04-18 22:09:20 +0300

    add authMiddleware for token-based user authorization

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,27 @@
+import { JWT_SECRET } from "../config/env";
+import User from "../models/usersModel";
+import jwt from "jsonwebtoken"
+
+export const authorize = async (req, res, next) => {
+  try{
+    let token;
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+      token = req.headers.authorization.split(" ")[1]
+    }
+    if (!token) {
+      const error = new Error("Not authorized, no token")
+      error.status = 401
+      throw error
+    }
+    const decoded = jwt.verify(token, JWT_SECRET)
+    const user = await User.findById(decoded.userId)
+    if (!user) {
+      return res.status(401).json({ success: false, message: "Unauthorized" })
+    }
+    req.user = user
+    next()
+
+  }catch(error){
+    next(error)
+  }
+}
