23c6ea40ae18e2ef41375b19e0170ce8bb4ebdfc
shuayb mohammed <shuaybmohammed244@gmail.com>
2026-04-18 22:09:07 +0300

    add userController with getAllUsers and getUser functions for user retrieval

---
+++ controllers/userController.js
@@ -0,0 +1,28 @@
+import User from "../models/usersModel";
+export const getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find();
+    if(!users){
+      const error = new Error("No users found")
+      error.status = 404
+      throw error
+    }
+    res.json({ success: true, message: "Users retrieved successfully", data: users });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id).select("-password");
+    if(!user){
+      const error = new Error("No user found")
+      error.status = 404
+      throw error
+    }
+    res.json({ success: true, message: "Users retrieved successfully", data: user });
+  } catch (error) {
+    next(error);
+  }
+};
